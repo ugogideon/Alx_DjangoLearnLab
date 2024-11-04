c36566bb517fa6c72c2dd6ca604c517484895326
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-04 16:25:54 +0000

    updates views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,5 +5,5 @@ from .models import Book
 
 def list_books(request):
     # Query all books from the database
-    books = Book.objects.all()  # Get all book instances
-    return render(request, 'list_books.html', {'books': books})
+    books = Book.objects.all()  # Retrieve all book instances
+    return render(request, 'list_books.html', {'books': books})  # Render the template with the book list
