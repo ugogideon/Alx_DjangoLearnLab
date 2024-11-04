7ca6e4c65fa791c05a9ac57223d07ef81fceaaa9
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-04 16:36:31 +0000

    updates views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from .views import list_books, LibraryDetailView
 
 urlpatterns = [
-    path('books/', list_books, name='list_books'),  # Function-based view
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # Class-based view
+    path('books/', list_books, name='list_books'),  # URL for the function-based view
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for the class-based view
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render
 from .models import Book
 
 def list_books(request):
-    # Query all books from the database
-    books = Book.objects.all()  # Retrieve all book instances
-    return render(request, 'list_books.html', {'books': books})  # Render the template with the book list
+    books = Book.objects.all()  # Retrieve all book instances from the database
+    return render(request, 'list_books.html', {'books': books})  # Render the list_books.html template with the context
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'  # Specify the template to use
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Add all books associated with the library to the context
+        return context
+        
