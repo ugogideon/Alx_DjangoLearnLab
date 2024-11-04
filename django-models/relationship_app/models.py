d7cab27285039824bfec2cd980efa41826c6e551
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-04 15:12:42 +0000

    updates

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,1 +1,9 @@
-test
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birthdate = models.DateField(null=True, blank=True)  # Optional field
+    nationality = models.CharField(max_length=50, null=True, blank=True)  # Optional field
+
+    def __str__(self):
+        return self.name
