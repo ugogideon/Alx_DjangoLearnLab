88ebeb3cc982ff5be3555cdf35223c8e1621524d
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-04 16:41:29 +0000

    updates views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,15 +2,15 @@
 
 from django.shortcuts import render
 from django.views import DetailView
-from .models import Book, Library
+from .models import Book, Library  # Import both Book and Library models
 
 def list_books(request):
     books = Book.objects.all()  # Retrieve all book instances from the database
-    return render(request, 'relationship_app/list_books.html', {'books': books})  # Use the correct template path
+    return render(request, 'relationship_app/list_books.html', {'books': books})  # Render the template with books
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'  # Use the correct template path
+    template_name = 'relationship_app/library_detail.html'  # Path to the library detail template
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
