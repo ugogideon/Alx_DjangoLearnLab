ecf58a5c05385c7443869749d1f62803d1361354
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-07 14:03:32 +0000

    Access control

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
+from .models import UserProfile
 
 def list_books(request):
     books = Book.objects.all()  # Retrieve all book instances from the database
@@ -35,24 +36,29 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
+# Check if the user is an Admin
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
+# Check if the user is a Liberian
 def is_librarian(user):
     return user.userprofile.role == 'Librarian'
 
+# Check if the user is a Member
 def is_member(user):
     return user.userprofile.role == 'Member'
 
+# Admin view (restricted to Admins)
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, 'admin_view.html')
 
+# Librarian view (restricted to Librarians)
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'librarian_view.html')
 
+# Member view (restricted to Members)
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'member_view.html')
-    
