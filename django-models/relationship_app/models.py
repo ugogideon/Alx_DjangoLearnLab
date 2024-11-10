d248cd9de356674239fcf99f458f22e6087a515d
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-10 16:46:47 +0000

    updates

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -48,3 +48,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+def __str__(self):
+        return self.name
+        
