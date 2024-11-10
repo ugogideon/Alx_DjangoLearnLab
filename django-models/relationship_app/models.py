032525884d8c11e07b0baa69d2b1b03b18acb7a8
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-10 18:15:16 +0000

    updates

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,30 +1,54 @@
 # models.py
 from django.db import models
-from relationship_app.models import Author  # Import the Author model
-from bookshelf.models import Book  # Import the Book model
-from .models import Library  # Import the Library model
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from relationship_app.models import Author  # Import the Author model
+from bookshelf.models import Book  # Import the Book model
+from .models import Library  # Import the Library model
 
+# Author Model
 class Author(models.Model):
     name = models.CharField(max_length=100)
     birthdate = models.DateField(null=True, blank=True)  # Optional field
     nationality = models.CharField(max_length=50, null=True, blank=True)  # Optional field
 
+    def __str__(self):
+        return self.name
+
+# Book Model with Custom Permissions
 class Book(models.Model):
     title = models.CharField(max_length=200)  # Title of the book
     author = models.ForeignKey(Author, on_delete=models.CASCADE)  # ForeignKey to Author
     publication_year = models.IntegerField()  # Publication year
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
+
+# Library Model
 class Library(models.Model):
     name = models.CharField(max_length=100)  # Name of the library
     books = models.ManyToManyField(Book)  # ManyToMany relationship to Book
 
+    def __str__(self):
+        return self.name
+
+# Librarian Model
 class Librarian(models.Model):
     name = models.CharField(max_length=100)  # Name of the librarian
     library = models.OneToOneField(Library, on_delete=models.CASCADE)  # OneToOne relationship to Library
 
+    def __str__(self):
+        return self.name
+
+# UserProfile Model for Role-based User Permissions
 class UserProfile(models.Model):
     ROLE_CHOICES = [
         ('Admin', 'Admin'),
@@ -34,12 +58,11 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
-    
- def __str__(self):
-     return self.user.username
- 
-# Signal to create a UserProfile when a new user is created
 
+    def __str__(self):
+        return self.user.username
+
+# Signal to create a UserProfile when a new user is created
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
@@ -48,7 +71,3 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
-
-def __str__(self):
-        return self.name
-        
