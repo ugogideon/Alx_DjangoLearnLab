3701d270cb4422c9a7026d863d639464470b9ca0
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-17 15:33:56 +0000

    updates

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from relationship_app.models import Author  # Assuming Author is in relationship_app
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')  # ForeignKey to Author author = models.CharField(max_length=100)
+    publication_year = models.PositiveIntegerField()
+    isbn = models.CharField(max_length=13, unique=True)  # Optional: ISBN for the book
 
     def __str__(self):
         return self.title
