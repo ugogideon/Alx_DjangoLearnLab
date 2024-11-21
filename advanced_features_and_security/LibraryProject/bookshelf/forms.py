a6f516057b36222dd5a7040199a3c566dca338d3
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-21 12:36:44 +0000

    updates

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from relationship_app.models import Book, Author
-
+from .models import Book
 # Book Form for Creating and Editing Books
 class BookForm(forms.ModelForm):
     class Meta:
@@ -11,7 +11,13 @@ class BookForm(forms.ModelForm):
             'author': 'Author',
             'publication_year': 'Year of Publication',
         }
-        
+
+class ExampleForm(forms.Form):
+    # Example form fields
+    title = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter book title'}))
+    author = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter author name'}))
+    publication_year = forms.IntegerField(required=True, widget=forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter publication year'}))
+       
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter book title'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
