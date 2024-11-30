6020102953cf537a40d469a9594cb0a35aeb0e05
Adoghe Gideon <ugogideon.35@gmail.com>
2024-11-30 10:28:49 +0000

    api works

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,8 +6,6 @@ from .serializers import BookSerializer
 # Creating my Booklist views here.
 
 class BookList(ListAPIView):
-    """
-    API view to retrieve the list of books.
-    """
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
